f54c04a54dd76a96c1808df215e394fc117bcdd5
Nomnivore <nominate110@gmail.com>
2021-04-21 13:37:53 -0400

    calculation func

---
+++ main.js
@@ -0,0 +1,21 @@
+const memory = {
+    first: 0,
+    last: 0,
+    operator: ''
+}
+
+function operate(num1, num2, oper) {
+    switch (oper) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "*":
+            return num1 * num2;
+        case "/":
+            return num1 / num2;
+        default:
+            return 0;
+    }
+}
+
