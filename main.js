41d1b3a65da988d1bbb6a982903815d248fe38be
Nomnivore <nominate110@gmail.com>
2021-04-22 14:44:32 -0400

    ascii operators

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ const memory = {
 const signs = {
     add: "+",
     subtract: "-",
-    multiply: "*",
-    divide: "/",
+    multiply: "×",
+    divide: "÷",
 };
 
 // html elements
@@ -26,17 +26,17 @@ function operate(num1, num2, oper) {
     num1 = parseFloat(num1);
     num2 = parseFloat(num2);
     switch (oper) {
-        case "+":
+        case signs.add:
             return (num1 + num2).toString();
-        case "-":
+        case signs.subtract:
             return (num1 - num2).toString();
-        case "*":
+        case signs.multiply:
             return (num1 * num2).toString();
-        case "/":
-            if (num2 === 0) return "ha haa."
+        case signs.divide:
+            if (num2 === 0) return "ha haa.";
             return (num1 / num2).toString();
         default:
-            return (0);
+            return 0;
     }
 }
 
@@ -46,12 +46,12 @@ function updateDisplay() {
     displayPrev.textContent = "";
 
     // update based on current state in memory
-    if (memory.solution !== '') {
-        console.log("ok")
+    if (memory.solution !== "") {
+        console.log("ok");
         // solution set by equals operator, or chaining operators
         display.textContent = memory.solution;
         displayPrev.textContent = `${memory.first} ${memory.operator} ${memory.last}`;
-        return
+        return;
     }
     if (memory.first && !memory.operator) {
         // during first number entry
@@ -60,7 +60,7 @@ function updateDisplay() {
     }
     if (memory.operator) {
         // during second number entry
-        displayPrev.textContent = `${memory.first} ${memory.operator}`;
+        displayPrev.innerHTML = `${memory.first} ${memory.operator}&nbsp;`;
         display.textContent = memory.last ? memory.last : "";
         return;
     }
@@ -69,14 +69,14 @@ function updateDisplay() {
 numbers.forEach(
     (num) =>
         (num.onclick = () => {
-            if (memory.solution !== '') {
+            if (memory.solution !== "") {
                 // reset solution
                 memory.solution = "";
-                memory.first = num.id
-                memory.last = 0
-                memory.operator = ''
-                updateDisplay()
-                return
+                memory.first = num.id;
+                memory.last = 0;
+                memory.operator = "";
+                updateDisplay();
+                return;
             }
 
             if (!memory.operator) {
@@ -98,53 +98,49 @@ numbers.forEach(
         })
 );
 
-opers.forEach(
-    (op) =>
-        (op.onclick = () => {
-            // act based on state again
-            if (memory.solution !== '') {
-                // following last operation
-                memory.first = memory.solution;
-                memory.solution = ""
-                memory.last = 0
-                memory.operator = signs[op.id];
-                updateDisplay();
-                return;
-            }
-            if (memory.first && !memory.last) {
-                // moves state to last num
-                memory.operator = signs[op.id];
-                updateDisplay();
-                return;
-            }
-            if (memory.first && memory.last) {
-                // chaining operations
-                memory.first = operate(
-                    memory.first,
-                    memory.last,
-                    memory.operator
-                );
-                memory.operator = signs[op.id];
-                memory.last = 0
-                updateDisplay();
-            }
-        })
-);
+opers.forEach((op) => {
+    op.textContent = signs[op.id]
+    op.onclick = () => {
+        // act based on state again
+        if (memory.solution !== "") {
+            // following last operation
+            memory.first = memory.solution;
+            memory.solution = "";
+            memory.last = 0;
+            memory.operator = signs[op.id];
+            updateDisplay();
+            return;
+        }
+        if (memory.first && !memory.last) {
+            // moves state to last num
+            memory.operator = signs[op.id];
+            updateDisplay();
+            return;
+        }
+        if (memory.first && memory.last) {
+            // chaining operations
+            memory.first = operate(memory.first, memory.last, memory.operator);
+            memory.operator = signs[op.id];
+            memory.last = 0;
+            updateDisplay();
+        }
+    };
+});
 
 document.getElementById("equals").onclick = (event) => {
-    if (!memory.operator) return
-    console.log('equals')
-//    first = (memory.first) ? memory.first : 0
-    memory.last = (memory.last) ? memory.last : memory.first
-    memory.solution = operate(memory.first, memory.last, memory.operator)
-    console.log(memory)
-    updateDisplay()
-}
+    if (!memory.operator) return;
+    console.log("equals");
+    //    first = (memory.first) ? memory.first : 0
+    memory.last = memory.last ? memory.last : memory.first;
+    memory.solution = operate(memory.first, memory.last, memory.operator);
+    console.log(memory);
+    updateDisplay();
+};
 
 document.getElementById("clear").onclick = (event) => {
-    memory.first = 0
-    memory.last = 0
-    memory.operator = ''
-    memory.solution = ''
-    updateDisplay()
-}
+    memory.first = 0;
+    memory.last = 0;
+    memory.operator = "";
+    memory.solution = "";
+    updateDisplay();
+};
